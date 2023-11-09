282859c778600a524ea307ddb433c7f9495032a5
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-11-09 15:36:23 +0530

    Finishing up

--- src/components/account/LoginDialog.jsx
+++ src/components/account/LoginDialog.jsx
@@ -10,7 +10,8 @@ import { AccountContext } from '../../context/AccountProvider';
 import { qrCodeImage } from '../../constants/data';
 
 const Component = styled(Box)`
-    display: flex; 
+    display: flex;
+ 
 `;
 
 const Container = styled(Box)`
@@ -26,8 +27,8 @@ const QRCOde = styled('img')({
 const Title = styled(Typography)`
     font-size: 26px;
     margin-bottom: 25px;
-    color: #525252;
-    font-family: Segoe UI,Helvetica Neue,Helvetica,Lucida Grande,Arial,Ubuntu,Cantarell,Fira Sans,sans-serif;
+    color: #0077b6; /* Change the color to blue */
+    font-family: Segoe UI, Helvetica Neue, Helvetica, Lucida Grande, Arial, Ubuntu, Cantarell, Fira Sans, sans-serif;
     font-weight: 300;
 `;
 
@@ -37,7 +38,7 @@ const StyledList = styled(List)`
         margin-top: 15px;
         font-size: 18px;
         line-height: 28px;
-        color: #4a4a4a;
+        // color: #4a4a4a;
     }
 `;
 
@@ -49,12 +50,12 @@ const dialogStyle = {
     maxHeight: '100%',
     borderRadius: 0,
     boxShadow: 'none',
-    overflow: 'hidden'
-}
+    overflow: 'hidden',
+    
+};
 
 const LoginDialog = () => {
-
-    const { setAccount,showloginButton, setShowloginButton, setShowlogoutButton } = useContext(AccountContext);
+    const { setAccount, showloginButton, setShowloginButton, setShowlogoutButton } = useContext(AccountContext);
 
     const onLoginSuccess = async (res) => {
         let decoded = jwt_decode(res.credential);
@@ -68,43 +69,37 @@ const LoginDialog = () => {
         console.log('Login Failed:', res);
     };
 
-    // const onSignoutSuccess = () => {
-    //     alert("You have been logged out successfully");
-    //     console.clear();
-    //     setShowloginButton(true);
-    //     setShowlogoutButton(false);
-    // };
-
     return (
         <Dialog
             open={true}
-            BackdropProps={{style: {backgroundColor: 'unset'}}}
+            BackdropProps={{ style: { backgroundColor: 'unset' } }}
             maxWidth={'md'}
             PaperProps={{ sx: dialogStyle }}
         >
             <Component>
                 <Container>
-                    <Title>To use WhatsApp on your computer:</Title>
+                    <Title>To use Collabshare on your computer:</Title> {/* Updated the title text */}
                     <StyledList>
-                        <ListItem>1. Open WhatsApp on your phone</ListItem>
-                        <ListItem>2. Tap Menu Settings and select WhatsApp Web</ListItem>
-                        <ListItem>3. Point your phone to this screen to capture the code</ListItem>
+                        <ListItem>1. Open Collabshare</ListItem> {/* Updated the list items */}
+                        <ListItem>2.Select Collabshare Web</ListItem>
+                        <ListItem>3.Sign in using your existing account</ListItem>
                     </StyledList>
                 </Container>
-                <Box style={{position:'relative'}}>
+                <Box style={{ position: 'relative' }}>
                     <QRCOde src={qrCodeImage} alt="QR Code" />
-                    <Box style={{position: 'absolute', top: '50%', transform: 'translateX(25%) translateY(-25%)'}}>
-                        { showloginButton ?
+                    <Box style={{ position: 'absolute', top: '50%', transform: 'translateX(25%) translateY(-25%)' }}>
+                        {showloginButton ? (
                             <GoogleLogin
                                 buttonText=""
                                 onSuccess={onLoginSuccess}
                                 onError={onLoginFailure}
-                            /> : null}
+                            />
+                        ) : null}
                     </Box>
                 </Box>
             </Component>
         </Dialog>
-    )
-}
+    );
+};
 
 export default LoginDialog;

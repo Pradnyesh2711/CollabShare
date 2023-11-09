a958ba81c52fc8e3941d8772b90bba8333c1d3d9
Pradnyesh2711 <100141334+Pradnyesh2711@users.noreply.github.com>
2023-11-09 15:36:04 +0530

    Finishing up

--- src/components/chat/chat/EmptyChat.jsx
+++ src/components/chat/chat/EmptyChat.jsx
@@ -1,26 +1,26 @@
 
 import { Box, styled, Typography, Divider } from '@mui/material';
 
-import { emptyChatImage } from '../../../constants/data';
 
 const Component = styled(Box)`
     background: #f8f9fa;
     padding: 30px 0;
     text-align: center;
     height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const Container = styled(Box)`
     padding: 0 200px;
+
 `;
     
-const Image = styled('img')({
-    marginTop: 100,
-    width: 400
-})
+
    
 const Title = styled(Typography)`
-    font-size: 32px;
+    font-size: 50px;
     font-family: inherit;
     font-weight: 300;
     color: #41525d;
@@ -44,10 +44,10 @@ const EmptyChat = () => {
     return (
         <Component>
             <Container>
-                <Image src={emptyChatImage} alt="empty" />
-                <Title>WhatsApp Web</Title>
-                <SubTitle>Now send and receive messages without keeping your phone online.</SubTitle>
-                <SubTitle>Use WhatsApp on up to 4 linked devices and 1 phone at the same time. </SubTitle>
+
+                <Title>CollabShare</Title>
+                <SubTitle>Now send and receive messages as well as files instantly.</SubTitle>
+                <SubTitle>Use Collabshare</SubTitle>
                 <StyledDivider />
             </Container>
         </Component>
